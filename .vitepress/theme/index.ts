d6a1d00c570648bd584e73c37726f3b8819d3685
FirstMegaGame4 <firstmegagame4@outlook.fr>
2025-08-17 03:33:28 +0200

    Theme Changes

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,14 +1,16 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
-import type { Theme } from 'vitepress'
+import {Theme, useData} from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
+    const { page, frontmatter } = useData();
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      "doc-before": frontmatter.value.title ? h("h1", { class: "vp-doc-header" }, frontmatter.value.title) : null
     })
   },
   enhanceApp({ app, router, siteData }) {
