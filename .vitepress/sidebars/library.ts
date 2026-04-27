6e1007a8954d90c0e0c5fe4211be2e6308c3cb8b
FirstMegaGame4 <firstmegagame4@outlook.fr>
2026-04-27 13:59:02 +0200

    Fill Sidebar

--- .vitepress/sidebars/library.ts
+++ .vitepress/sidebars/library.ts
@@ -16,5 +16,97 @@ export default [
             { text: "Resource Key Attachments", link: "/library/core/rka" },
             { text: "Registry Companions", link: "/library/core/registry-companions" }
         ]
+    },
+    {
+        text: "Config",
+        items: [
+            { text: "Creating a Config for your Mod", link: "/library/config" },
+            { text: "Default Config Element Types", link: "/library/config/default-element-types" },
+            { text: "Custom Config Element Types", link: "/library/config/custom-element-types" }
+        ]
+    },
+    {
+        text: "Item",
+        items: [
+            { text: "Creating and Registering Items", link: "/library/item" },
+            { text: "Item Categories", link: "/library/item/categories" },
+            { text: "Custom Item Properties", link: "/library/item/custom-properties" },
+            { text: "Catalog", link: "/library/item/catalog" }
+        ]
+    },
+    {
+        text: "Block",
+        items: [
+            { text: "Creating and Registering Blocks", link: "/library/block" },
+            { text: "Block Wrappers", link: "/library/block/wrappers" },
+            { text: "Custom Block Properties", link: "/library/block/custom-properties" },
+            { text: "Catalog", link: "/library/block/catalog" }
+        ]
+    },
+    {
+        text: "DataGen",
+        items: [
+            { text: "Extended Data Generator Entrypoints", link: "/library/datagen/entrypoints" },
+            { text: "Data Manager & Automated Data Generation", link: "/library/datagen/automation" },
+            { text: "Default Data Handlers", link: "/library/datagen/handlers" },
+            { text: "Processor Catalog", link: "/library/datagen/processor-catalog" }
+        ]
+    },
+    {
+        text: "Fluid",
+        items: [
+            { text: "Creating and Registering Flowable Fluids", link: "/library/fluid" },
+            { text: "Advanced Flowable Fluids", link: "/library/fluid/advanced" },
+            { text: "Fluid Properties", link: "/library/fluid/properties" }
+        ]
+    },
+    {
+        text: "WoodSet",
+        items: [
+            { text: "Creating and Registering Wood Sets", link: "/library/woodset" },
+            { text: "Registering Boat Entity Renderers", link: "/library/woodset/boats" }
+        ]
+    },
+    {
+        text: "WorldGen",
+        items: [
+            { text: "Feature Packs & Replication", link: "library/worldgen/feature-packs" },
+            { text: "Seed Independence", link: "library/worldgen/seed-independence" },
+            { text: "Vein Generation", link: "library/worldgen/vein-generation" }
+        ]
+    },
+    {
+        text: "Rendering",
+        items: [
+            { text: "Entity Model Utilities", link: "library/rendering/entity-models" },
+            { text: "Cosmetic Management & Renderers", link: "library/rendering/cosmetic" },
+            { text: "Texture Aliases", link: "library/rendering/texture-aliases" }
+        ]
+    },
+    {
+        text: "Network",
+        items: [
+            { text: "Friend Byte Buff Extensions", link: "library/network/buf-extensions" },
+            { text: "Network Handlers", link: "library/network/handlers" },
+            { text: "Delayed Network", link: "library/network/delayed" }
+        ]
+    },
+    {
+        text: "Java",
+        items: [
+            { text: "Reference", link: "/library/java" }
+        ]
+    },
+    {
+        text: "Math",
+        items: [
+            { text: "Reference", link: "/library/math" }
+        ]
+    },
+    {
+        text: "Mod Integrations",
+        items: [
+            { text: "Trinkets Updated", link: "/library/mod-integration/trinkets-updated" }
+        ]
     }
 ]
---
+++ docs/library/java/index.md
@@ -0,0 +1,17 @@
+---
+title: MModding Java Module
+---
+
+This part of the documentation is focused on utility objects that do not depend on Minecraft's content.
+
+This module evolves occasionally, when MModding devs think of things to abstract.
+
+Let's give a list of these objects. Those will be detailed in their respective entries:
+
+- The [`Color`](./colors.md) object and their extensions.
+- The container objects such as `Unit`, `Pair`, etc...
+- The [`Either`](./eithers.md) object.
+- The [functional interfaces](./finterfaces).
+- The [list variants](./lists).
+- The [map variants](./maps).
+- The [object property interfaces](./opinterfaces).
